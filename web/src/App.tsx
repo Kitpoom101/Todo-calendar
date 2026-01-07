ca30a5346abc4836f7853651cecbee3280bce059
kitpoom <poom14.kt@gmail.com>
2026-01-07 13:27:09 +0700

    change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useTestData } from "../src/hooks/getData" // use { } to get utility[hooks, ...] none for component
-import TodoCard from './component/todoCard';
+import TodoCard from './component/TodoCard';
+
 function App() {
   const { data } = useTestData();
   return (
